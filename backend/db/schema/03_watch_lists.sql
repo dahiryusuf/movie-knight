f0ac56e69b618fd8976413fad09fc176ba7e222f
dahiryusuf <dahir.yusuf@hotmail.com>
2022-01-26 02:19:27 -0500

    init

--- backend/db/schema/03_watch_lists.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS watch_lists CASCADE;
-
-CREATE TABLE watch_lists(
-    id SERIAL PRIMARY KEY NOT NULL,
-    movie_id VARCHAR(255) NOT NULL,
-    poster_path VARCHAR(255),
-    user_id INTEGER REFERENCES users(id)
-);
--- backend/db/schema/05_movie_lists.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS movie_lists CASCADE;
-
-CREATE TABLE movie_lists(
-    id SERIAL PRIMARY KEY NOT NULL,
-    movie_id VARCHAR(255) NOT NULL,
-    watch_party_id INTEGER REFERENCES watch_parties(id) ON DELETE CASCADE
-);
